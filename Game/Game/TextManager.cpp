08512a873af8147b06706e4bc2d1b47d5a29c3c3
dogok0532 <dogok0000@naver.com>
2018-07-06 10:01:18 +0900

    XXXXXX

--- Game/Game/TextManager.cpp
+++ Game/Game/TextManager.cpp
@@ -5,9 +5,56 @@ IMPLEMENT_SINGLETON(CTextManager)
 
 CTextManager::CTextManager()
 {
+	HRESULT hr = D2D1CreateFactory(
+		D2D1_FACTORY_TYPE_SINGLE_THREADED,
+		&m_pD2DFactory
+	);
+
+	if (SUCCEEDED(hr))
+	{
+		hr = DWriteCreateFactory(
+			DWRITE_FACTORY_TYPE_SHARED,
+			__uuidof(IDWriteFactory),
+			reinterpret_cast<IUnknown**>( &m_pDWriteFactory)
+		);
+	}
+
+	HRESULT hr = m_pDWriteFactory->CreateTextFormat(
+		L"Gabriola",                // Font family name.
+		NULL,                       // Font collection (NULL sets it to use the system font collection).
+		DWRITE_FONT_WEIGHT_REGULAR,
+		DWRITE_FONT_STYLE_NORMAL,
+		DWRITE_FONT_STRETCH_NORMAL,
+		72.0f,
+		L"en-us",
+		&m_pTextFormat
+	);
+
+	if (SUCCEEDED(hr))
+	{
+		hr = m_pTextFormat->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_CENTER);
+	}
+
+	if (SUCCEEDED(hr))
+	{
+		hr = m_pTextFormat->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_CENTER);
+	}
+
 }
 
 
 CTextManager::~CTextManager()
 {
 }
+
+void CTextManager::SetText(wstring strText)
+{
+	m_strText = strText;
+	m_iTextLength = strText.length();
+}
+
+void CTextManager::SetFormat()
+{
+
+}
+
--- Game/Game/TextManager.h
+++ Game/Game/TextManager.h
@@ -1,8 +1,26 @@
 #pragma once
 #include "Defines.h"
+#include <DWrite.h>
+#include <d2d1.h>
+
 class CTextManager
 {
+private:
+	IDWriteFactory* m_pDWriteFactory;
+	IDWriteTextFormat* m_pTextFormat;
+	
+	
+	wstring m_strText;
+	UINT32 m_iTextLength;
 
+	ID2D1Factory* m_pD2DFactory;
+	ID2D1HwndRenderTarget* m_pRT;
+	ID2D1SolidColorBrush* m_pBlackBrush;
+
+
+public:
+	void SetText(wstring);
+	void SetFormat();
 
 
 	DECLARE_SINGLETON(CTextManager)
